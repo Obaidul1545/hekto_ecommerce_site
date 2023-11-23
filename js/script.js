0dfaeb77cdac50c7a8bde455e1ec92bd63e33c89
Obaidul1545 <mdobaidulislam128@gmail.com>
2023-11-23 22:21:35 +0600

    slider added

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Products slider js
+$(".products_slide").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  arrows: false,
+  dots: true,
+});
+
+// mixitup js
+var mixer = mixitup(".my_mixitup");
